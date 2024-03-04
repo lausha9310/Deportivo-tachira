2dd2a856666d610058b92bb32e2e74021e584d79
Gabriel <gagm2909@gmail.com>
2024-03-04 16:05:13 -0300

    fuction new

--- validar.js
+++ validar.js
@@ -7,4 +7,24 @@ function validar (){
     if(nombre === ""){ 
         alert("El campo nombre esta vacio");
     }
+  if(apellido === ""){
+    alert("El campo apellido esta vacio");
+  }
+  if(comentario === ""){
+    alert("El campo comentario esta vacio")
+  };
+  // Obtener el formulario
+  var reset = document.getElementById('myform');
+  var miToast = document.getElementById('miToast');
+
+  // Agregar un controlador de eventos para el evento de envío del formulario
+  reset.addEventListener('submit', function(event) {
+      // Evitar que el formulario se envíe de forma predeterminada
+      event.preventDefault();
+    var toast = new bootstrap.Toast(miToast);
+    toast.show();
+
+      // Restablecer el formulario después de enviarlo
+    myform.reset();
+  });
 }
