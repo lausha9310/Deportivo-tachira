8baee9fef4863d7aabc720ce496ccde4c0c6c75a
Gabriel <gagm2909@gmail.com>
2024-03-12 10:24:38 -0300

    nueva funcion

--- validar.js
+++ validar.js
@@ -1,18 +1,33 @@
-function validar (){
-    var nombre, apellido, comentario;
-    nombre = document.getElementById("nombre").value; 
-    apellido = document.getElementById("apellido").value;
-    comentario = document.getElementById("comentario").value;
+function validar() {
+  var nombre, apellido, comentario;
+  nombre = document.getElementById("nombre").value;
+  apellido = document.getElementById("apellido").value;
+  comentario = document.getElementById("comentario").value;
 
-    if(nombre === ""){ 
-        alert("El campo nombre esta vacio");
-    }
-  if(apellido === ""){
-    alert("El campo apellido esta vacio");
+  function validetionNombre(nombre) {
+      return /^[a-zA-Z]+$/.test(nombre); // Utilizamos una expresión regular para verificar si solo contiene letras
   }
-  if(comentario === ""){
-    alert("El campo comentario esta vacio")
-  };
+
+  if (!validetionNombre(nombre)) {
+      alert("El campo nombre contiene caracteres no permitidos");
+      return; // Detener la ejecución si el nombre no es válido
+  }
+
+  if (nombre === "") {
+      alert("El campo nombre está vacío");
+      return;
+  }
+
+  if (apellido === "") {
+      alert("El campo apellido está vacío");
+      return;
+  }
+
+  if (comentario === "") {
+      alert("El campo comentario está vacío");
+      return;
+  }
+
   // Obtener el formulario
   var reset = document.getElementById('myform');
   var miToast = document.getElementById('miToast');
@@ -21,10 +36,10 @@ function validar (){
   reset.addEventListener('submit', function(event) {
       // Evitar que el formulario se envíe de forma predeterminada
       event.preventDefault();
-    var toast = new bootstrap.Toast(miToast);
-    toast.show();
+      var toast = new bootstrap.Toast(miToast);
+      toast.show();
 
       // Restablecer el formulario después de enviarlo
-    myform.reset();
+      myform.reset();
   });
 }
